9f5a9ae9e06397a95487c6b951cf0bedea4ba50f
Bianca Aspin <bianca@mj94.net>
2024-01-12 02:17:08 -0800

    Add swap() solution.

--- Day_01/Day_01.py
+++ Day_01/Day_01.py
@@ -1,1 +1,15 @@
 # Challenge: Create a program that swaps the values of two variables.
+
+def swap(a, b):
+    # temp = a
+    # a = b
+    # b = temp
+    # return a, b
+
+    a, b = b, a
+    return a,b
+
+var1 = 1
+var2 = 2
+
+print(swap(var1, var2))
