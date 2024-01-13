a481e379fb1b22142db7543d83e64630d7c84790
Bianca Aspin <bianca@mj94.net>
2024-01-12 20:12:37 -0800

    Add print statements for debugging and illustration purposes.

--- Day_01/Day_01.py
+++ Day_01/Day_01.py
@@ -1,15 +1,16 @@
 # Challenge: Create a program that swaps the values of two variables.
 
 def swap(a, b):
-    # temp = a
-    # a = b
-    # b = temp
-    # return a, b
-
     a, b = b, a
     return a,b
 
-var1 = 1
-var2 = 2
+var1 = True
+var2 = False
 
-print(swap(var1, var2))
+print("Var1 after function:", var1)
+print("Var2 after function:", var2)
+
+var1, var2 = swap(var1, var2)
+
+print("Var1 after function:", var1)
+print("Var2 after function:", var2)
