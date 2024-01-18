5f548563dee56a901b86302036bfda5876bbc468
Bianca Aspin <bianca@mj94.net>
2024-01-17 22:49:14 -0800

    Add additional code and tests for advanced features.

--- Day_08/Day_08.py
+++ Day_08/Day_08.py
@@ -1,14 +1,50 @@
 # Write a function that accepts a string and calculates the number of uppercase and lowercase letters in it.
 
-def letter_count(str):
-    count = {"uppercase": 0, "lowercase": 0}
+# def char_overview(str):
+#     count = {"uppercase": 0, "lowercase": 0}
+
+#     for char in str:
+#         code = ord(char)
+#         if code >= 65 and code <= 90:
+#             count["uppercase"] += 1
+#         elif code >= 97 and code <= 122:
+#             count["lowercase"] += 1
+
+#     return count
+
+# OK, but what if I want more details, like:
+# - how many numbers there are
+# - how many spaces there are
+# - how many additional characters there are
+# BUT I don't want to see a category if it's 0.
+
+def char_overview(str):
+    count = {
+        "uppercase": 0, 
+        "lowercase": 0, 
+        "number": 0, 
+        "space": 0, 
+        "other": 0
+    }
 
     for char in str:
         code = ord(char)
-        if code >= 65 and code <= 90:
+        if code == 32:
+            count["space"] += 1
+        elif code >= 48 and code <= 57:
+            count["number"] += 1
+        elif code >= 65 and code <= 90:
             count["uppercase"] += 1
         elif code >= 97 and code <= 122:
             count["lowercase"] += 1
+        else:
+            count["other"] += 1
+        
+    for key in count.copy():
+        if count[key] == 0:
+            del count[key]
+
+    if len(count) == 0:
+        return None
 
     return count
-
--- Day_08/test_day_08.py
+++ Day_08/test_day_08.py
@@ -1,13 +1,38 @@
-from Day_08 import letter_count
+from Day_08 import char_overview
 
 def test_word_count():
     """Given a single word string, return a dictionary containing a count of lowercase and uppercase letters."""
     str = "Python"
-    result = letter_count(str)
+    result = char_overview(str)
     assert result == {"uppercase": 1, "lowercase": 5}
 
 def test_sentence_count():
     """Given a sentence, return a dictionary containing a count of lowercase and uppercase letters."""
     str = "I love Python"
-    result = letter_count(str)
-    assert result == {"uppercase": 2, "lowercase": 9}
+    result = char_overview(str)
+    assert result == {"uppercase": 2, "lowercase": 9, "space": 2}
+
+def test_empty():
+    """Given an empty string, return None"""
+    str = ""
+    result = char_overview(str)
+    assert result == None
+
+def test_space_only():
+    """Given a string containing only spaces, return a dictionary containing a count of spaces."""
+    str = "     "
+    result = char_overview(str)
+    assert result == {"space": 5}
+
+def test_numbers_only():
+    """Given a string containing only numbers, return a dictionary containing a count of numbers."""
+    str = "12345"
+    result = char_overview(str)
+    assert result == {"number": 5}
+
+def test_other_only():
+    """Given a string containing only special characters and punctuation, return a dictionary containing a count of other."""
+    str = "!!*$%#@^()"
+    result = char_overview(str)
+    assert result == {"other": 10}
+
