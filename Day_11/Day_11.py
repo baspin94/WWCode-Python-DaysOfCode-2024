f258d26da82ed61638f58755a11f28395a80a015
Bianca Aspin <bianca@mj94.net>
2024-01-23 02:24:35 -0800

    Add ValueError test, update existing tests, update code to print tables.

--- Day_11/Day_11.py
+++ Day_11/Day_11.py
@@ -1,15 +1,26 @@
 # Write a program to print the multiplication table of a given number.
 # Input: Number to base table off of, optional upper limit of table (default to 12)
+# What if we want to specify a lower limit as well?
+# What if we want it to look like an actual table?
+
+from tabulate import tabulate
 
 def xtable(number, upper_limit = 12, lower_limit = 0):
     if type(number) is not int:
         raise TypeError('Input must be an integer.')
+    if lower_limit >= upper_limit:
+        raise ValueError('Upper limit must be larger than lower limit.')
 
     table = []
     for val in range(lower_limit, upper_limit + 1):
         product = number * val
-        table.append(product)
+        table.append((val, product))
+    
+    print(tabulate(table, headers = ["x", number], tablefmt="grid"))
+
     return table
 
-# What if we want to specify a lower limit as well?
-# What if we want it to look like an actual table?
+xtable(12)
+xtable(10, 10)
+xtable(10, 20, 11)
+
--- Day_11/test_day_11.py
+++ Day_11/test_day_11.py
@@ -5,14 +5,42 @@ def test_xtable_no_limit():
     """Given a number and no specified upper limit, return a list of products for that number multiplied from 0-12."""
     number = 12
     result = xtable(number)
-    assert result == [0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120, 132, 144]
+    expected = [
+        (0, 0), 
+        (1, 12),
+        (2, 24),
+        (3, 36),
+        (4, 48),
+        (5, 60),
+        (6, 72), 
+        (7, 84),
+        (8, 96),
+        (9, 108),
+        (10, 120), 
+        (11, 132),
+        (12, 144)
+    ]
+    assert result == expected
 
 def test_xtable_w_up_limit():
     """Given a number and a specified upper limit, return a list of products for that number multiplied from 0-limit."""
     number = 10
     upper = 10
     result = xtable(number, upper)
-    assert result == [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    expected = [
+        (0, 0),
+        (1, 10),
+        (2, 20),
+        (3, 30), 
+        (4, 40), 
+        (5, 50), 
+        (6, 60),
+        (7, 70), 
+        (8, 80),
+        (9, 90), 
+        (10, 100)
+    ]
+    assert result == expected
 
 def test_xtable_w_low_limit():
     """Given a number and a specified lower limit, return a list of products for that number multiplied from lower-upper limit."""
@@ -20,11 +48,32 @@ def test_xtable_w_low_limit():
     lower = 11
     upper = 20
     result = xtable(number, upper, lower)
-    assert result == [110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
+    expected = [
+        (11, 110),
+        (12, 120),
+        (13, 130),
+        (14, 140),
+        (15, 150), 
+        (16, 160),
+        (17, 170),
+        (18, 180),
+        (19, 190), 
+        (20, 200)
+    ]
+    assert result == expected
 
-def test_xtable_error():
+def test_xtable_type_error():
     """Given an invalid input, return a TypeError with message 'Input must be integer'."""
     string = "oops"
     with pytest.raises(TypeError) as excinfo:
         xtable(string)
     assert str(excinfo.value) == 'Input must be an integer.'
+
+def test_xtable_value_error():
+    """Given an upper limit that is smaller than the lower limit, return an error."""
+    number = 10
+    lower = 20
+    upper = 11
+    with pytest.raises(ValueError) as excinfo:
+        xtable(number, upper, lower)
+    assert str(excinfo.value) == 'Upper limit must be larger than lower limit.'
