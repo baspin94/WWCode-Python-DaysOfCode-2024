aa49208ff7c2ec583d7841e15b9c662f5a889f4f
Bianca Aspin <bianca@mj94.net>
2024-01-23 01:59:22 -0800

    Add/update tests and code to include a lower limit for table.

--- Day_11/Day_11.py
+++ Day_11/Day_11.py
@@ -1,14 +1,15 @@
 # Write a program to print the multiplication table of a given number.
 # Input: Number to base table off of, optional upper limit of table (default to 12)
 
-def xtable(number, upper_limit = 12):
+def xtable(number, upper_limit = 12, lower_limit = 0):
     if type(number) is not int:
         raise TypeError('Input must be an integer.')
 
     table = []
-    for val in range(upper_limit + 1):
+    for val in range(lower_limit, upper_limit + 1):
         product = number * val
         table.append(product)
     return table
 
+# What if we want to specify a lower limit as well?
 # What if we want it to look like an actual table?
--- Day_11/test_day_11.py
+++ Day_11/test_day_11.py
@@ -7,12 +7,20 @@ def test_xtable_no_limit():
     result = xtable(number)
     assert result == [0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120, 132, 144]
 
-def test_xtable_w_limit():
+def test_xtable_w_up_limit():
     """Given a number and a specified upper limit, return a list of products for that number multiplied from 0-limit."""
     number = 10
-    limit = 10
-    result = xtable(number, limit)
+    upper = 10
+    result = xtable(number, upper)
     assert result == [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+def test_xtable_w_low_limit():
+    """Given a number and a specified lower limit, return a list of products for that number multiplied from lower-upper limit."""
+    number = 10
+    lower = 11
+    upper = 20
+    result = xtable(number, upper, lower)
+    assert result == [110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
 
 def test_xtable_error():
     """Given an invalid input, return a TypeError with message 'Input must be integer'."""
